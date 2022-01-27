fd8261ffe57d9dc89d90649a326f38a5b58b30bd
Roger Rangel <roger.rangel.ee@gmail.com>
2022-01-27 17:10:34 +0100

    Integrate React Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React, { Component } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { Component, useEffect } from 'react';
+import { Routes, Route } from "react-router-dom";
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
+
+
+
 class App extends Component {
   render() {
-    console.log(generatePalette(seedColors[4]));
+   
+    const PaletteList = () => <h1>PALETTE LIST GOES HERE</h1>;
+    const IndividualPalette = () => <h1>INDIVIDUAL PALETTE</h1>;
+
     return (
-      <div>
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+
+      <Routes>
+        <Route
+          path="/"
+          element={(  
+             <PaletteList />
+          )} />
+        <Route
+          path="/palette/:id"
+          element={(  
+             <IndividualPalette />
+          )} />
+      </Routes>
+      
+ 
+      //  <Palette palette={generatePalette(seedColors[4])} />
     );
   }
 }
