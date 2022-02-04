12c58dbf1fd09847b33c227057afed5a00039562
Roger Rangel <roger.rangel.ee@gmail.com>
2022-02-04 14:37:48 +0100

    Start styling PaletteList

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,18 +1,46 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import seedColors from './seedColors';
 import MiniPalette from "./MiniPalette";
+import { makeStyles } from '@mui/styles';
 
+
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    color: 'white',
+  },
+  nav: {
+    display: "flex",
+    width: "100%",
+    justifyContent: "space-between",
+    color: "white"
+  },
+  palettes: {
+    boxSizing: "border-box",
+    width: "100%",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 30%)",
+    gridGap: "5%"
+  }
+});
 export default function PaletteList() {
   const palettes = seedColors;
+  const classes = useStyles();
   
   return (
-    <div>
-      <MiniPalette />
-        <h1>React Colors</h1>
-        {palettes.map(palette => {
-          return <MiniPalette {...palette} />
-        })}
+    <div className={classes.root}>
+      <div className={classes.container}>
+        <nav className={classes.nav}>
+          <h1>React Colors</h1>
+        </nav>
+        <div className={classes.palettes}>
+              {palettes.map(palette => (
+                <MiniPalette {...palette} />
+              ))}
+        </div>
+    </div>
     </div>
   )
 }
