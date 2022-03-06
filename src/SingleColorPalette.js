5d8d96496cc1ce161b2cf78ec9e49a80099f79e5
Roger Rangel <roger.rangel.ee@gmail.com>
2022-03-06 14:20:05 +0100

    Displaying shades in SingleColorPalette - 1

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,14 +1,25 @@
 import React, {Component} from 'react';
+import { useParams } from 'react-router-dom';
+import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
-class SingleColorPalette extends Component {
-    render() {
+function SingleColorPalette() {
+    const { paletteId } = useParams(); //useParams to get the :paletteId
+ 
+    const findPalette = (paletteId) => {
+        return seedColors.find(function (palette) {
+          return palette.id === paletteId;
+        });
+      };
+
+    const palette = generatePalette(findPalette(paletteId));
 
         return(
             <div>
                 <h1>Single Color Palette</h1>
             </div>
         )
-    }
+    
 }
 
 export default SingleColorPalette
